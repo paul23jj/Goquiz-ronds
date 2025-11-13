78b3f6a87ede69dc6a84ebd179c689c08a957e06
Paul Gaulmin <paul.gaulmin@ynov.com>
2025-11-13 11:11:20 +0100

    ajout de questions temporaires pour cyber et ia

--- quizIAdata.go
+++ quizIAdata.go
@@ -4,5 +4,35 @@ import "fmt"
 
 func StartQuizIAData() {
 	fmt.Println("=== Quiz IA et Data ===")
-	// plus tard : poser les questions ici
+	fmt.Println("=== Quiz Intelligence Artificielle et Data Science ===")
+
+	totalQuestions := 3
+	score := 0
+
+	// Question 1
+	fmt.Println("Quel algorithme est couramment utilisé pour les tâches de classification en apprentissage automatique ?")
+	fmt.Println("1. Régression linéaire")
+	fmt.Println("2. Forêt aléatoire")
+	fmt.Println("3. K-means")
+	if CheckAnswer(2) {
+		score++
+	}
+
+	// Question 2
+	fmt.Println("Quel langage de programmation est le plus populaire pour l'analyse de données ?")
+	fmt.Println("1. R")
+	fmt.Println("2. Java")
+	fmt.Println("3. C++")
+	if CheckAnswer(1) {
+		score++
+	}
+	// Question 3
+	fmt.Println("Que signifie le terme 'Big Data' ?")
+	fmt.Println("1. Données volumineuses et complexes")
+	fmt.Println("2. Données de petite taille")
+	fmt.Println("3. Données non structurées uniquement")
+	if CheckAnswer(1) {
+		score++
+	}
+	CalculateScore(score, totalQuestions)
 }
--- quizcyber.go
+++ quizcyber.go
@@ -4,5 +4,34 @@ import "fmt"
 
 func StartQuizCyber() {
 	fmt.Println("=== Quiz Cyber Sécurité ===")
-	// plus tard : poser les questions ici
+	
+	totalQuestions := 3
+	score := 0
+	// Question 1
+	fmt.Println("Quel protocole est utilisé pour sécuriser les communications sur Internet ?")
+	fmt.Println("1. HTTP")
+	fmt.Println("2. FTP")
+	fmt.Println("3. HTTPS")
+	if CheckAnswer(3) {
+		score++
+	}
+	// Question 2
+	fmt.Println("Qu'est-ce qu'un pare-feu (firewall) ?")
+	fmt.Println("1. Un logiciel de traitement de texte")
+	fmt.Println("2. Un dispositif de sécurité réseau")
+	fmt.Println("3. Un type de virus informatique")
+	if CheckAnswer(2) {
+		score++
+	}
+	// Question 3
+	fmt.Println("Quelle est la meilleure pratique pour créer un mot de passe sécurisé ?")
+	fmt.Println("1. Utiliser des mots courants")
+	fmt.Println("2. Utiliser une combinaison de lettres, chiffres et symboles")
+	fmt.Println("3. Utiliser son nom ou sa date de naissance")
+	if CheckAnswer(2) {
+		score++
+	}
+	CalculateScore(score, totalQuestions)
 }
+
+
