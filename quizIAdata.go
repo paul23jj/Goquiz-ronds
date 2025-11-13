ff94a6b8e21e7313fd06181884d04862ce5dedc1
Paul Gaulmin <paul.gaulmin@ynov.com>
2025-11-13 12:41:36 +0100

    ajout de 7 nouvelles question pour la rubrique info

--- quizIAdata.go
+++ quizIAdata.go
@@ -6,7 +6,7 @@ func StartQuizIAData() {
 	fmt.Println("=== Quiz IA et Data ===")
 	fmt.Println("=== Quiz Intelligence Artificielle et Data Science ===")
 
-	totalQuestions := 3
+	totalQuestions := 10
 	score := 0
 
 	// Question 1
@@ -32,6 +32,30 @@ func StartQuizIAData() {
 	fmt.Println("2. Données de petite taille")
 	fmt.Println("3. Données non structurées uniquement")
 	if CheckAnswer(1) {
+		score++
+	}
+	// Question 4
+	fmt.Println("Que signifie 'Machine Learning' ?")
+	fmt.Println("1. Apprentissage automatique")
+	fmt.Println("2. Apprentissage manuel")
+	fmt.Println("3. Apprentissage supervisé uniquement")
+	if CheckAnswer(1) {
+		score++
+	}
+	// Question 5
+	fmt.Println("Que signiefie SQL ?")
+	fmt.Println("1. Structured Query Language")
+	fmt.Println("2. Simple Query Language")
+	fmt.Println("3. Sequential Query Language")
+	if CheckAnswer(1) {
+		score++
+	}
+	// Question 6
+	fmt.Println("Que signifie le réseau de neurones ?")
+	fmt.Println("1. Un protocole de communication")
+	fmt.Println("2. Un type de réseau informatique")
+	fmt.Println("3. Un modèle d'apprentissage inspiré du cerveau humain")
+	if CheckAnswer(3) {
 		score++
 	}
 	CalculateScore(score, totalQuestions)
